e356a516d7c8a31d031f7668746ae1d57b291896
gatsby-fe <gatsby-fe@gate.me>
2024-11-25 23:01:14 +0800

    feat: update

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { registerRootComponent } from 'expo';
-import { Provider as JotaiProvider } from 'jotai';
+import { registerRootComponent } from "expo";
+import { Provider as JotaiProvider } from "jotai";
+import { NativeWindStyleSheet } from "nativewind";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { RootNavigator } from './navigation/RootNavigator';
+import { RootNavigator } from "./navigation/RootNavigator";
 
 const queryClient = new QueryClient();
 
@@ -21,5 +19,9 @@ export default function App() {
     </QueryClientProvider>
   );
 }
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 registerRootComponent(App);
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { Button } from '@rneui/themed';
-import { useNavigation } from '@react-navigation/native';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../navigation/types';
+import React from "react";
 
-type HomeScreenNavigationProp = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+import { useTailwind } from "nativewind";
+import { StyleSheet, Text, View, ViewStyle } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { Button } from "@rneui/themed";
+
+import { RootStackParamList } from "../navigation/types";
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "Home"
+>;
 
 const HomeScreen = () => {
   const navigation = useNavigation<HomeScreenNavigationProp>();
+  const btnStyle = StyleSheet.flatten(
+    useTailwind({
+      className: "mb-8",
+    })
+  ) as ViewStyle;
 
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold mb-4">Welcome Home</Text>
+      <Text className="text-2xl font-bold mb-20">Welcome Home</Text>
       <Button
         title="Go to Profile"
-        onPress={() => navigation.navigate('Profile', { userId: '1' })}
-        className="mb-2"
+        onPress={() => navigation.navigate("Profile", { userId: "1" })}
+        containerStyle={btnStyle}
       />
       <Button
         title="Go to Settings"
-        onPress={() => navigation.navigate('Settings')}
+        onPress={() => navigation.navigate("Settings")}
       />
     </View>
   );
