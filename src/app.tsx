465ebb7d4ff55ae864fc979a9330fd9fe1e488cc
gatsby-fe <gatsby-fe@gate.me>
2024-12-17 23:34:14 +0800

    fix: i18n

--- src/app.tsx
+++ src/app.tsx
@@ -5,21 +5,19 @@ import { Provider as JotaiProvider } from 'jotai';
 import { NativeWindStyleSheet } from 'nativewind';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { I18nextProvider } from 'react-i18next';
 
 import { RootNavigator } from './navigation/RootNavigator';
 
-import i18n from '@/i18n';
+import { useSetI18nFromStorage } from '@/i18n';
 
 const queryClient = new QueryClient();
 
 export default function App() {
+  useSetI18nFromStorage();
   return (
     <QueryClientProvider client={queryClient}>
       <JotaiProvider>
-        <I18nextProvider i18n={i18n}>
-          <RootNavigator />
-        </I18nextProvider>
+        <RootNavigator />
       </JotaiProvider>
     </QueryClientProvider>
   );
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -3,17 +3,35 @@ import { initReactI18next } from 'react-i18next';
 import zh from './locales/zh';
 import en from './locales/en';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
 const loadLanguage = async () => {
   const language = await AsyncStorage.getItem('app_language');
   return language || 'zh';
 };
 
-const initI18n = async () => {
-  const defaultLanguage = await loadLanguage();
+export const useSetI18nFromStorage = () => {
+  useState(() => {
+    initI18n();
+  });
+  useEffect(() => {
+    async function tmp() {
+      try {
+        const storedLng = await loadLanguage();
+        if (storedLng) {
+          i18n.changeLanguage(storedLng);
+        }
+      } catch (error) {
+        console.error('Failed to load language:', error);
+      }
+    }
+    tmp();
+  }, []);
+};
 
+const initI18n = async () => {
   i18n.use(initReactI18next).init({
-    lng: defaultLanguage,
+    lng: 'zh',
     resources: {
       zh: {
         translation: zh,
@@ -28,7 +46,5 @@ const initI18n = async () => {
     },
   });
 };
-
-initI18n();
 
 export default i18n;
